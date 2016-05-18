4aaf59c4f7b5f1ca2308a9cfcbf669c3c20e52a2
Angela837 <837063499@qq.com>
2016-05-18 22:33:58 +0800

    chatroom1.1

--- src/com/chatroom/TestSockClient.java
+++ src/com/chatroom/TestSockClient.java
@@ -8,39 +8,43 @@ import java.util.Scanner;
 
 public class TestSockClient {
 	public static void main(String[] args) {
-		DataOutputStream dos=null;
-		DataInputStream dis=null;
-		String str=null;
-		Socket socket=null;
-		Scanner input=new Scanner(System.in);
-		
-	    try {
-                socket =new Socket("127.0.0.1",8888);
-             do{
-            	dis=new DataInputStream(socket.getInputStream());
-	    		dos=new DataOutputStream(socket.getOutputStream());
-	    		
-	    	    str=input.nextLine();
-	    	  
-	    	   dos.writeUTF(str);
-	    		if((str=dis.readUTF())!=null){
-		    	  System.out.println(str); 
-	    		}
-	    		 
-	    	}while(!str.equals("88"));
-	    	
+		DataOutputStream dos = null;
+		DataInputStream dis = null;
+		String str = null;
+		String s = null;
+		Socket socket = null;
+		Scanner input = new Scanner(System.in);
+
+		try {
+			socket = new Socket("127.0.0.1", 8888);
+
+			do {
+				dis = new DataInputStream(socket.getInputStream());
+				dos = new DataOutputStream(socket.getOutputStream());
+
+				s = input.nextLine();
+
+				dos.writeUTF(s);
+				if ((str = dis.readUTF()) != null) {
+					System.out.println(str);
+				}
+				
+
+			} while (!s.equals("88"));
+			
 		} catch (IOException e) {
-		System.out.println("客户端结束！");
-		}finally{
-			//关闭
+			System.out.println("客户端结束！");
+		} finally {
+			// 关闭
 			try {
 				dos.close();
 				dis.close();
 				socket.close();
 			} catch (IOException e) {
 				e.printStackTrace();
-			}		}
-		
+			}
+		}
+
 	}
 
 }
