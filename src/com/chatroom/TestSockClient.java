1a4b925aef95e57169333d5a994006050a2360be
NIIT <NIIT@406-0180>
2016-05-19 21:22:44 +0800

    CHATROOM 1.2

--- src/com/chatroom/TestSockClient.java
+++ src/com/chatroom/TestSockClient.java
@@ -7,33 +7,42 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TestSockClient {
+	static DataInputStream dis = null;
+
 	public static void main(String[] args) {
 		DataOutputStream dos = null;
-		DataInputStream dis = null;
 		String str = null;
-		String s = null;
 		Socket socket = null;
 		Scanner input = new Scanner(System.in);
 
 		try {
-			socket = new Socket("127.0.0.1", 8888);
 
+			socket = new Socket("127.0.0.1", 8888);
 			do {
 				dis = new DataInputStream(socket.getInputStream());
 				dos = new DataOutputStream(socket.getOutputStream());
+				// 匿名内部类 另开线程 显示服务器传来的内容
+				new Thread() {
+					@Override
+					public void run() {
+						String s = null;
+						try {
+							if ((s = dis.readUTF()) != null) {
+								System.out.println(s);
+							}
 
-				s = input.nextLine();
+						} catch (Exception e) {
+							// TODO: handle exception
+						}
+					}
+				}.start();
+				str = input.nextLine();
+				dos.writeUTF(str);
 
-				dos.writeUTF(s);
-				if ((str = dis.readUTF()) != null) {
-					System.out.println(str);
-				}
-				
-
-			} while (!s.equals("88"));
-			
-		} catch (IOException e) {
+			} while (!str.equals("88"));
 			System.out.println("客户端结束！");
+
+		} catch (IOException e) {
 		} finally {
 			// 关闭
 			try {
--- src/com/chatroom/TestSockServer.java
+++ src/com/chatroom/TestSockServer.java
@@ -5,43 +5,80 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestSockServer {
+	// 主线程
+	static List<Client> clientList = new ArrayList<Client>();// 存线程存通道socket
+
 	public static void main(String[] args) {
-		DataOutputStream dos=null;
-		DataInputStream dis=null;
-		Socket socket=null;
-	    try {
-			//准备
-	    	ServerSocket s=new ServerSocket(8888);
-	    	System.out.println("服务器启动！");
-	    	//连接
-	    	socket=s.accept();
-	    	//信息交互
-	    	while(true){
-	    		dos=new DataOutputStream(socket.getOutputStream());
-	    		dis=new DataInputStream(socket.getInputStream());
-	    		String str=null;
-	    		if((str=dis.readUTF())!=null){
-	    			System.out.println("客户端："+str);
-	    			//System.out.println("客户端IP："+socket.getInetAddress().getHostAddress());
-	    			//System.out.println("客户端端口号："+socket.getPort());
-	    		}
-	    		dos.writeUTF("服务器说："+str);
-	    	}
-	    	
+		Socket socket = null;
+		try {
+			// 准备
+			ServerSocket s = new ServerSocket(8888);
+			System.out.println("服务器启动！");
+			// 连接
+			// socket = s.accept();
+			// 信息交互等待
+			while (true) {
+				socket = s.accept();
+				Client c = new TestSockServer().new Client(socket);// 一个c代表一个客户端对象
+				clientList.add(c);
+				c.start();
+			}
 		} catch (IOException e) {
-		System.out.println("服务器结束！");
-		}finally{
-			//关闭
+			System.out.println("服务器结束！");
+		}
+
+	}
+
+	// 多线程 一人一通道
+	class Client extends Thread {
+		DataOutputStream dos = null;
+		DataInputStream dis = null;
+		Socket socket = null;// 通道
+
+		public Client(Socket socket) {
+			super();
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
 			try {
-				dis.close();
-				dos.close();
-				socket.close();
+				while (true) {
+					dos = new DataOutputStream(socket.getOutputStream());
+					dis = new DataInputStream(socket.getInputStream());
+					String str = null;
+					if ((str = dis.readUTF()) != null) {
+						System.out.println("客户端：" + str);
+						// System.out.println("客户端IP："+socket.getInetAddress().getHostAddress());
+						// System.out.println("客户端端口号："+socket.getPort());
+					}
+					// dos.writeUTF(socket.getPort() + "说：" + str);
+					for (int i = 0; i < clientList.size(); i++) {
+						// a的话返回给bcd
+						new DataOutputStream(clientList.get(i).socket.getOutputStream())
+								.writeUTF(socket.getPort() + ":" + str);
+						;
+					}
+				}
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}		}
-		
+			} finally {
+				// 关闭
+				try {
+					dis.close();
+					dos.close();
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 }
